86cbe06a67ac84f6517231bd82fc3a06061cb967
hengin-eer <hirokix0608@gmail.com>
2025-04-16 05:50:53 +0900

    🐛 #7 依存関係にremark-mathとrehype-katexを追加し、markdown設定を更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  markdown: {
-    shikiConfig: {
-      theme: "github-dark",
-      langs: [],
-    },
-  },
-  site: "https://hengin-eer.github.io/",
-  vite: {
-    optimizeDeps: {
-      noDiscovery: true,
-      include: [],
-    },
-  },
+	integrations: [tailwind()],
+	markdown: {
+		shikiConfig: {
+			theme: "github-dark",
+			langs: [],
+		},
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
+	site: "https://hengin-eer.github.io/",
+	vite: {
+		optimizeDeps: {
+			noDiscovery: true,
+			include: [],
+		},
+	},
 });
