b35bcf1a21d960ba0c8efca7bfa865a1d1f868b5
hengin-eer <hirokix0608@gmail.com>
2025-08-13 18:44:51 +0900

    🎨 #16 カラースキーマを追加

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,13 @@ export default {
 		colors: {
 			"primary-green": "#18ef95",
 			"primary-dark-green": "#00b86b",
+			"primary-emerald": "#34D399",
 			"primary-blue": "#13aed0",
 			"primary-red": "#f6444f",
 			white: "#ffffff",
 			"gray-base": "#9ebcca",
 			"gray-light": "#edf0f7",
+			"gray-dark": "#547B8D",
 			black: "#000000",
 		},
 		extend: {},
