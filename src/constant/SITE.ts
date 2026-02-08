24dd2eb0a79c92e456315931af89fb9c6631092c
timdaik <hirokix0608@gmail.com>
2026-02-09 01:05:26 +0900

    add: サイト設定を定数に切り出し

---
+++ src/constant/SITE.ts
@@ -0,0 +1,5 @@
+const SITE_TITLE = "tomodaction";
+const SITE_DESCRIPTION = "Portfolio of Hiroki Tomoda (timdaik).";
+const SITE_URL = "https://hiro-to.moda";
+
+export { SITE_TITLE, SITE_DESCRIPTION, SITE_URL };
