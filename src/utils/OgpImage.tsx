b9d0670082454643a39b162a4d8994a9169edd60
timdaik <hirokix0608@gmail.com>
2026-02-10 01:04:48 +0900

    update: トップページのOGP画像を作成 画像を読み込むためのBase64形式に変換する関数も作った

--- src/utils/OgpImage.tsx
+++ src/utils/OgpImage.tsx
@@ -1,8 +1,11 @@
 import satori from "satori";
 import { Resvg } from "@resvg/resvg-js";
+import "node:fs";
 import type { ReactNode } from "react";
 import type { CollectionEntry } from "astro:content";
 import "../styles/global.css";
+import { SITE_TITLE } from "src/constant/SITE";
+import { readFileSync } from "node:fs";
 
 async function GenerateOgpImage(children: ReactNode) {
 	const svg = await satori(children, {
@@ -18,7 +21,7 @@ async function GenerateOgpImage(children: ReactNode) {
 				style: "normal",
 			},
 		],
-		debug: true,
+		// debug: true,
 	});
 
 	const resvg = new Resvg(svg, {
@@ -33,6 +36,13 @@ async function GenerateOgpImage(children: ReactNode) {
 	return pngData;
 }
 
+function readImageAsDataURL(imagePath: string): string {
+	const image_base64 = readFileSync(new URL(imagePath, import.meta.url), {
+		encoding: "base64",
+	});
+	return `data:image/png;base64,${image_base64}`;
+}
+
 export function generateBlogOgpImage(blog: CollectionEntry<"blog">) {
 	const { cover, title, tag, updatedAt } = blog.data;
 	return GenerateOgpImage(
@@ -42,7 +52,7 @@ export function generateBlogOgpImage(blog: CollectionEntry<"blog">) {
 				flexDirection: "column",
 			}}
 		>
-			<img src={cover.src} alt={title} />
+			<img src={readImageAsDataURL(cover.src)} alt={title} />
 			<p>{tag}</p>
 			<h1>{title}</h1>
 			<p>{updatedAt}</p>
@@ -53,15 +63,70 @@ export function generateBlogOgpImage(blog: CollectionEntry<"blog">) {
 export function generateDefaultOgpImage() {
 	return GenerateOgpImage(
 		<div
-			className="flex flex-col"
-			// style={{
-			// 	display: "flex",
-			// 	flexDirection: "column",
-			// }}
+			style={{
+				padding: "60px 100px",
+				width: "1200px",
+				height: "630px",
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "space-between",
+				backgroundColor: "white",
+				// backgroundImage: "linear-gradient(to right, #13aed0, white)",
+				backgroundImage: "linear-gradient(to right, white 50%, #13aed0)",
+				color: "#1a2550",
+			}}
 		>
-			<h1>tomodaction</h1>
-			<h2>my mode, action</h2>
-			<p>Portfolio of Hiroki Tomoda (timdaik).</p>
+			<div
+				style={{
+					marginTop: 120,
+					display: "flex",
+					flexDirection: "column",
+					gap: 64,
+					fontFamily: "Averia Serif Libre",
+				}}
+			>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						gap: 12,
+					}}
+				>
+					<span
+						style={{
+							fontSize: 88,
+							color: "#13aed0",
+						}}
+					>
+						{SITE_TITLE};
+					</span>
+					<span
+						style={{
+							fontSize: 64,
+						}}
+					>
+						my mode, action
+					</span>
+				</div>
+
+				<p
+					style={{
+						color: "#00b86b",
+						fontSize: 40,
+					}}
+				>
+					&copy; hiro-to.moda
+				</p>
+			</div>
+			<img
+				style={{
+					height: 360,
+					width: 360,
+					borderRadius: "50%",
+				}}
+				src={readImageAsDataURL("../../public/favicon.png")}
+				alt="Avatar"
+			/>
 		</div>,
 	);
 }
