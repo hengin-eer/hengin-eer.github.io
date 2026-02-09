684b021d7589f3897be385dc155c51c1b619d428
timdaik <hirokix0608@gmail.com>
2026-02-09 18:44:36 +0900

    add: 一般とブログ向けにOGP画像を生成するエンドポイント作成

---
+++ src/pages/blog/ogp.[slug].png.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+import { generateBlogOgpImage } from "src/utils/OgpImage";
+
+export const GET: APIRoute = async ({ params }) => {
+	const slug = params.slug;
+	const blogs = await getCollection("blog");
+	const blog = blogs.find((blog) => blog.slug === slug);
+	const png = generateBlogOgpImage(blog!);
+
+	return new Response(png, {
+		headers: {
+			"Content-Type": "image/png",
+		},
+	});
+};
+
+export async function getStaticPaths() {
+	const blogs = await getCollection("blog");
+	return blogs.map((blog) => ({
+		params: { slug: blog.slug },
+	}));
+}
