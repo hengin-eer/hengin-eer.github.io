7767b97bd4918dad7a0c5b3db275195960ceb9b1
timdaik <hirokix0608@gmail.com>
2026-09-16 03:27:28 +0900

    fix(storybook): define navigator in portable tests

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -6,5 +6,6 @@ import * as projectAnnotations from "./preview";
 global.window = new Window({ url: "https://localhost:8080" }) as never;
 global.document = global.window.document;
 global.Element = Element as never;
+global.navigator = global.window.navigator as never;
 
 setProjectAnnotations([projectAnnotations]);
